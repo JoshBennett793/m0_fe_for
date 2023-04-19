8cac65840c01acab47efcde1102287b3935fe220
Josh Bennett <joshbennett793@yahoo.com>
2023-04-19 15:56:08 -0500

    Complete challenge 1

--- for.js
+++ for.js
@@ -4,7 +4,11 @@
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
 
-
+for (var i = 25; i < 75; i++) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+}
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
